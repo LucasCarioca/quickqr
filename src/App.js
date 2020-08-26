12863a4d1522d6786fedb7a11d6a0012456b6085
Lucas Desouza <lucasfds2777@gmail.com>
2020-08-26 15:35:34 -0500

    material ui

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
 import React from 'react';
 import { upload } from './service/skynet.service';
 import './App.css';
+import { 
+  Container, 
+  Button, 
+  Divider, 
+  Grid, 
+  Paper, 
+  CircularProgress,
+  AppBar,
+  Toolbar,
+  Typography
+} from '@material-ui/core';
 
 const QRCode = require('qrcode.react');
 
@@ -8,28 +19,37 @@ class App extends React.Component {
    
   state = { 
     selectedFile: null,
-    url: null
+    url: null,
+    loading: false,
+    imgSrc: null
   }; 
 
   onFileChange = event => { 
+    const reader = new FileReader();
+    reader.onload = (e) => {
+      this.setState({imgSrc: e.target.result})
+    }
     this.setState({ selectedFile: event.target.files[0] }); 
+    reader.readAsDataURL(event.target.files[0]);
   }; 
    
   onFileUpload = () => { 
+    this.setState({loading: true});
     upload(this.state.selectedFile).then(link => {
+      this.setState({loading: false});
       console.log(link);
       this.setState({url: `https://www.siacdn.com/${link}`})
     }).catch(error => {
+      this.setState({loading: false});
       console.error(error);
     })
   }; 
 
   fileData = () => { 
-   
     if (this.state.selectedFile) { 
-        
       return ( 
-        <div> 
+        <> 
+          <Divider style={{margin: '15px 0'}}/>
           <h2>File Details:</h2> 
           <p>File Name: {this.state.selectedFile?.name}</p> 
           <p>File Type: {this.state.selectedFile?.type}</p> 
@@ -37,38 +57,67 @@ class App extends React.Component {
             Last Modified:{" "} 
             {this.state.selectedFile?.lastModifiedDate?.toDateString()} 
           </p> 
-        </div> 
+          <img style={{maxWidth: '100%'}} src={this.state.imgSrc}/>
+        </> 
       ); 
-    } else { 
-      return ( 
-        <div> 
-          <br /> 
-          <h4>Choose before Pressing the Upload button</h4> 
-        </div> 
-      ); 
-    } 
+    }
+    return null;
   }; 
    
   render() { 
-   
-    return ( 
-      <div> 
-          <h1> 
-            QuickQr
-          </h1> 
-          <h3> 
-            File Upload using React! 
-          </h3> 
-          <div> 
-              <input type="file" onChange={this.onFileChange} /> 
-              <button onClick={this.onFileUpload}> 
-                Upload! 
-              </button> 
-          </div> 
-        {this.fileData()} 
-        {this.state.url != null ? <QRCode value={this.state.url} /> : null}
-      </div> 
-    ); 
+    return ( <>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" style={{flexGrow: 1}}>
+            QuickQrCode
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Container>
+        <h1> 
+          
+        </h1> 
+        <Grid container spacing={3}>
+          <Grid item xs={4}>
+            <Paper style={{padding: '15px'}}>
+              {this.state.selectedFile == null ? (
+                <>
+                  <h2>To begin, select a file</h2>
+                  <Button
+                    variant="outlined"
+                    component="label"
+                    color="primary"
+                  >
+                    Select File
+                  <input type="file" style={{ display: "none" }} onChange={this.onFileChange} /> 
+                  </Button>
+                </>
+              ) : (
+              <>
+                <h2>Your file is ready</h2>
+                <p>Click the button below to create upload the file and create a QR code.</p>
+                {this.state.loading ? <div style={{padding: '15px', display: 'flex', justifyContent: 'center'}}><CircularProgress /></div>
+                : <Button variant="contained" color="primary" onClick={this.onFileUpload}> Generate Code! </Button>}
+              </>
+              )}
+              {this.fileData()} 
+            </Paper>
+          </Grid>
+          <Grid item xs={8}>
+            {this.state.url != null ? (
+            <Paper style={{padding: '15px', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}}>
+              <h2>
+                Your QR Code is ready!! 🎉
+              </h2>
+              <div style={{padding: '15px', display: 'flex', justifyContent: 'center'}}>
+                <QRCode value={this.state.url} />
+              </div>
+            </Paper>
+            ) : null}
+          </Grid>
+        </Grid>
+      </Container> 
+    </>); 
   } 
 } 
 
