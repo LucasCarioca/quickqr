ff80063d3515db14ccfb77cdef90dda0a71fb0e4
Lucas Desouza <lucasfds2777@gmail.com>
2020-08-29 17:33:48 -0500

    login with google firebase

--- src/App.js
+++ src/App.js
@@ -1,15 +1,45 @@
-import React from 'react';
+import React, {useContext, useEffect, useState} from 'react';
 import './App.css';
-import {AppBar, Container, Toolbar, Typography, Button} from '@material-ui/core';
+import {AppBar, Button, Container, LinearProgress, Toolbar, Typography} from '@material-ui/core';
 import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 import AddCircleIcon from '@material-ui/icons/AddCircle';
 import HomeIcon from '@material-ui/icons/Home';
-import {BrowserRouter as Router, Route, Switch, Link} from "react-router-dom";
+import {BrowserRouter as Router, Link, Route, Switch} from "react-router-dom";
 import CodeResultsPage from "./pages/CodeResultsPage";
 import CreateCodePage from "./pages/CreateCodePage";
 import Landing from "./pages/Landing";
+import {FirebaseContext} from "./firebase";
 
 const App = () => {
+    const firebase = useContext(FirebaseContext);
+    const [user, setUser] = useState(firebase.user);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        firebase.auth.getRedirectResult().then(res => {
+            setLoading(false);
+            firebase.user = res?.user || null;
+            firebase.credential = res?.credential || null;
+            setUser(firebase.user);
+        }).catch(error => {
+            console.error(error);
+        });
+        console.log(firebase.auth.currentUser);
+    }, [firebase])
+
+    const signOut = () => {
+        firebase.auth.signOut().then(() => {
+            firebase.user = null;
+            firebase.credential = null;
+            setUser(null);
+        }).catch(error => {
+            console.error(error);
+        });
+    }
+
+    if (loading) return (
+        <LinearProgress/>
+    )
     return (
         <Router>
             <AppBar position="static">
@@ -18,8 +48,13 @@ const App = () => {
                         QuickQrCode
                     </Typography>
                     <Button component={Link} to="/" color="inherit"><HomeIcon/></Button>
-                    <Button component={Link} to="/code"color="inherit"><AddCircleIcon/></Button>
-                    <Button component={Link} to="/login"color="inherit"><AccountCircleIcon/></Button>
+                    <Button component={Link} to="/code" color="inherit"><AddCircleIcon/></Button>
+                    {user === null ? (
+                        <Button onClick={firebase.authenticateWithGoogle}
+                                color="inherit"><AccountCircleIcon/></Button>) : (
+                        <Button onClick={signOut}><img style={{width: '25px', borderRadius: '12px'}}
+                                                       src={user.photoURL} alt={user.displayName}/></Button>
+                    )}
                 </Toolbar>
             </AppBar>
             <br/>
@@ -39,8 +74,6 @@ const App = () => {
         </Router>
     );
 }
-
-
 
 
 export default App;
--- src/firebase.js
+++ src/firebase.js
@@ -1,22 +1,32 @@
 import app from 'firebase/app';
+import 'firebase/auth';
 import React from 'react';
 
 export const FirebaseContext = React.createContext(null);
 
 export const config = {
-    apiKey: process.env.API_KEY,
-    authDomain: process.env.AUTH_DOMAIN,
-    databaseURL: process.env.DATABASE_URL,
-    projectId: process.env.PROJECT_ID,
-    storageBucket: process.env.STORAGE_BUCKET,
-    messagingSenderId: process.env.MESSAGING_SENDER_ID,
-    appId: process.env.APP_ID,
-    measurementId: process.env.MEASUREMENT_ID,
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_DATABASE_URL,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
+    measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
 class Firebase {
     constructor() {
         app.initializeApp(config);
+        this.AUTH_LS_KEY = "firebaseAuth";
+        this.auth = app.auth();
+        this.user = null;
+        this.credential = null;
+        this.googleProvider = new app.auth.GoogleAuthProvider();
+    }
+
+    authenticateWithGoogle = () => {
+        this.auth.signInWithRedirect(this.googleProvider);
     }
 }
 
